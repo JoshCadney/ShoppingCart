3cacfc1bdad6ad6095cf28cdc8824edbedd17d34
unknown <JDC@INDIANELEPHANT.zoo.lan>
2014-09-04 17:51:28 +0100

    Price calculator works

--- ShoppingCart.Tests/PriceCalculatorTests.cs
+++ ShoppingCart.Tests/PriceCalculatorTests.cs
@@ -3,6 +3,7 @@
     using FakeItEasy;
     using FluentAssertions;
     using NUnit.Framework;
+    using System.Collections.Generic;
     using System.Linq;
 
     [TestFixture]
@@ -29,6 +30,46 @@
 
             // Then
             price.Should().Be(0);
+        }
+
+        [Test]
+        public void An_apple_should_cost_fifty()
+        {
+            A.CallTo(() => cart.Items).Returns(new List<string> { "A99" });
+
+            var price = calculator.CalculatePrice();
+
+            price.Should().Be(50);
+        }
+
+        [Test]
+        public void Three_apples_should_cost_130()
+        {
+            A.CallTo(() => cart.Items).Returns(new List<string> { "A99", "A99", "A99" });
+
+            var price = calculator.CalculatePrice();
+
+            price.Should().Be(130);
+        }
+
+        [Test]
+        public void Five_Biscuits_Cost_120()
+        {
+            A.CallTo(() => cart.Items).Returns(new List<string> { "B15", "B15", "B15", "B15", "B15" });
+
+            var price = calculator.CalculatePrice();
+
+            price.Should().Be(120);
+        }
+
+        [Test]
+        public void Order_of_items_does_not_matter()
+        {
+            A.CallTo(() => cart.Items).Returns(new List<string> { "A99", "B15", "A99", "B15", "A99" });
+
+            var price = calculator.CalculatePrice();
+
+            price.Should().Be(175);
         }
     }
 }
--- ShoppingCart/PriceCalculator.cs
+++ ShoppingCart/PriceCalculator.cs
@@ -1,15 +1,38 @@
 ﻿namespace ShoppingCart
 {
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
 
     public interface IPriceCalculator
     {
         int CalculatePrice();
     }
 
+    public class Offer
+    {
+        public readonly string Item;
+        public readonly int NumberRequiredForDeal;
+        public readonly int Saving;
+
+        public Offer(string item, int numberRequiredForDeal, int saving)
+        {
+            Item = item;
+            NumberRequiredForDeal = numberRequiredForDeal;
+            Saving = saving;
+        }
+    }
+
     public class PriceCalculator : IPriceCalculator
     {
         private readonly ICart cart;
+        private readonly Dictionary<string, int> pricesDictionary = new Dictionary<string, int> { { "A99", 50 }, { "B15", 30 }, { "C40", 60 }, { "T34", 99 } };
+
+        private readonly List<Offer> specialOffers = new List<Offer>
+                                                     {
+                                                         new Offer("A99", 3, 20),
+                                                         new Offer("B15", 2, 15)
+                                                     };
 
         public PriceCalculator(ICart cart)
         {
@@ -18,7 +41,38 @@
 
         public int CalculatePrice()
         {
-            throw new NotImplementedException();
+            var totalsDictionary = CreateTotalsDictionary();
+
+            return CostWithoutSpecialOffers(totalsDictionary) - Savings(totalsDictionary);
+        }
+
+        private Dictionary<String, int> CreateTotalsDictionary()
+        {
+            var items = cart.Items;
+            var runningTotalsDictionary = new Dictionary<string, int>();
+
+            foreach (var item in items)
+            {
+                if (runningTotalsDictionary.ContainsKey(item))
+                {
+                    runningTotalsDictionary[item]++;
+                }
+                else
+                {
+                    runningTotalsDictionary.Add(item, 1);
+                }
+            }
+            return runningTotalsDictionary;
+        }
+
+        private int CostWithoutSpecialOffers(Dictionary<string, int> totalsDictionary)
+        {
+            return totalsDictionary.Sum(product => pricesDictionary[product.Key] * product.Value);
+        }
+
+        private int Savings(Dictionary<string, int> totalsDictionary)
+        {
+            return specialOffers.Where(offer => totalsDictionary.ContainsKey(offer.Item)).Sum(offer => (totalsDictionary[offer.Item] / offer.NumberRequiredForDeal) * offer.Saving);
         }
     }
 }
